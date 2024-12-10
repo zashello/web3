cfe16e20b258e661918466f921465d4db8d9f8db
zashello <shallowdream@duck.com>
2024-12-10 21:53:45 +0800

    1

--- test/test.sol
+++ test/test.sol
@@ -1,2 +1,11 @@
 // SPDX-License-Identifier:MIT
 pragma solidity ^0.8.20;
+contract Hello {
+    string strvar = "hello,worls";
+    function say() public view returns (string memory) {
+        return strvar;
+    }
+    function set(string memory newh) public {
+        strvar = newh;
+    }
+}
