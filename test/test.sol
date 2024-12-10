7145183a48a768e63e8c3031fab57b04a8e2b0b4
zashello <shallowdream@duck.com>
2024-12-10 22:07:09 +0800

    renew

--- test/test.sol
+++ test/test.sol
@@ -1,11 +1,14 @@
 // SPDX-License-Identifier:MIT
 pragma solidity ^0.8.20;
 contract Hello {
-    string strvar = "hello,worls";
+    string strvar = "hello world";
     function say() public view returns (string memory) {
-        return strvar;
+        return addinfo(strvar);
     }
     function set(string memory newh) public {
         strvar = newh;
+    }
+    function addinfo(string memory hwd) internal pure returns(string memory) {
+    return string .concat(hwd," from zion");
     }
 }
