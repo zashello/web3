4c607b75f6cf3f1d1d4c3e3d3e4136bf81465983
zashello <shallowdream@duck.com>
2024-12-11 22:11:53 +0800

    update

--- helloworld/test2/test.sol
+++ helloworld/test2/test.sol
@@ -2,4 +2,7 @@
 pragma solidity ^0.8.20;
 contract helloworld {
     string public greeting = "helloworld";
+    function show() public view returns(string memory){
+        return(greeting);
+    }
 }
