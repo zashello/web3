aa5296aa2ad5b5e16c379d3d15cc1edd5d707c76
zashello <shallowdream@duck.com>
2024-12-12 23:15:58 +0800

    little

--- variable/test/teat.sol
+++ variable/test/teat.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.20;
 contract variable {
     string public hello = "hello,world"; //blockchain区块链上
 
-    function dosome() public view returns (uint,address,int){
+    function dosome() public view returns(uint,address,int){
         int numa = type(int).min; //内存变量，只在调用该函数时使用
         uint time = block.timestamp;  //全局变量
         address sender = msg.sender;   //同上
