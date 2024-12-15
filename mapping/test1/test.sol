42da4c3e35237a712290e74095fad341bc102abe
zashello <shallowdream@duck.com>
2024-12-15 18:13:02 +0800

    mpping

--- mapping/test1/test.sol
+++ mapping/test1/test.sol
@@ -1,2 +1,11 @@
 // SPDX-License-Identifier:MIT
 pragma solidity ^0.8.20;
+contract Mapping{
+    mapping (address => uint) public mymap;
+    function get(address _add) public view returns(uint){
+        return mymap[_add];
+    }
+    function set(address _add,uint _numa) public {
+        mymap[_add] = _numa;
+    }
+}
