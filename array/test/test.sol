4c786d80f7b171fd6a605a6817f05f9377c48d2a
zashello <shallowdream@duck.com>
2024-12-16 18:00:00 +0800

    1

--- array/test/test.sol
+++ array/test/test.sol
@@ -1,2 +1,16 @@
 // SPDX-License-Identifier:MIT
 pragma solidity ^0.8.20;
+contract Array {
+    uint[] public arr;
+    uint[] public arr1 = [1,2,3];
+    uint[10] public arr2;
+    function set(uint _i) public {
+        arr1.push(_i);
+    }
+    function get() public view returns(uint[] memory){
+       return arr1;
+    }
+    function pop() public {
+        arr1.pop();
+    }
+    }
