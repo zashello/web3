0e73a2c04cffea702c1eb29a175a0705ecb8045f
zashello <shallowdream@duck.com>
2024-12-17 22:32:24 +0800

    1

--- array/test/test.sol
+++ array/test/test.sol
@@ -4,13 +4,16 @@ contract Array {
     uint[] public arr;
     uint[] public arr1 = [1,2,3];
     uint[10] public arr2;
-    function set(uint _i) public {
+    function set(uint _i) public {//在末尾添加一个元素
         arr1.push(_i);
     }
-    function get() public view returns(uint[] memory){
+    function get() public view returns(uint[] memory){//显示arr1中含有的元素
        return arr1;
     }
-    function pop() public {
+    function pop() public {//去除最后一个元素
         arr1.pop();
+    }
+    function delarr(uint _index) public{
+        delete arr1[_index]; //将指定其位置的值变回为初始值
     }
     }
