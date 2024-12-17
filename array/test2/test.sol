51fa159b4118d128732fb9bf798ddc58b0be5632
zashello <shallowdream@duck.com>
2024-12-17 23:06:46 +0800

    1

--- array/test2/test.sol
+++ array/test2/test.sol
@@ -1,15 +1,15 @@
 // SPDX-License-Identifier:MIT
 pragma solidity ^0.8.20;
 contract rem0ve{
-    uint[] public a = [1,2,3,4,5];
-    function remove(uint _index) public {
+    uint[] public a = [1,2,3,5,6];
+    function remove(uint _index) public {//从将要去除的第i号元素开始，直达最后一位，所有数字等于后一位数字，最后pop掉最后一个元素
         require(_index < a.length,"this index is too long");
         for (uint i = _index;i < a.length-1;i++){
             a[i] = a[i + 1];
         }
         a.pop();       
     }
-    function get() public view returns(uint[] memory){
+    function get() public view returns(uint[] memory){//显示该数组
         return a;
     }
 }
