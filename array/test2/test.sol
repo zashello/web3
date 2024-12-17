4f03e2d75ac756c189495a7bcf7abe2d51cacd70
zashello <shallowdream@duck.com>
2024-12-17 23:02:09 +0800

    remove

--- array/test2/test.sol
+++ array/test2/test.sol
@@ -1,2 +1,15 @@
 // SPDX-License-Identifier:MIT
 pragma solidity ^0.8.20;
+contract rem0ve{
+    uint[] public a = [1,2,3,4,5];
+    function remove(uint _index) public {
+        require(_index < a.length,"this index is too long");
+        for (uint i = _index;i < a.length-1;i++){
+            a[i] = a[i + 1];
+        }
+        a.pop();       
+    }
+    function get() public view returns(uint[] memory){
+        return a;
+    }
+}
